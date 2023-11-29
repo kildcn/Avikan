75fd1d3e0420373bfb8c9497580bc4a2355b8121
Killian Le Doucen <killian.ledoucen@yahoo.fr>
2023-11-29 12:05:01 +0100

    bird model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_28_094656) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_29_110303) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "birds", force: :cascade do |t|
+    t.string "common_name"
+    t.string "scientific_name"
+    t.text "description"
+    t.string "habitat"
+    t.string "conservation_status"
+    t.boolean "migrates"
+    t.integer "experience_points"
+    t.string "common_location"
+    t.string "bird_habitat_type"
+    t.string "bird_size"
+    t.string "diet"
+    t.integer "max_velocity"
+    t.string "rarity"
+    t.string "sound_url"
+    t.integer "weight"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
