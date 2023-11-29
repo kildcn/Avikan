593320a3fa767f96d9932116165e078f0ac0a622
rebeccabri <brisloowrebecca@gmail.com>
2023-11-29 13:05:09 +0100

    Add username, date_joined, avatar_url to User model & Update Seeds w/ Users 🌱

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,55 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning Database 🧼"
+User.destroy_all
+
+# Create Users
+User.create!(
+  username: 'Rebecca',
+  email: 'rebecca@test.com',
+  password: '123456',
+  date_joined: Date.today,
+  avatar_url: "https://kitt.lewagon.com/placeholder/users/rebeccabri"
+)
+
+User.create!(
+  username: 'Alberto',
+  email: 'alberto@example.com',
+  password: '123456',
+  date_joined: Date.today,
+  avatar_url: "https://kitt.lewagon.com/placeholder/users/agalara"
+)
+
+User.create!(
+  username: 'Killian',
+  email: 'killian@example.com',
+  password: '123456',
+  date_joined: Date.today,
+  avatar_url: "https://kitt.lewagon.com/placeholder/users/kildcn"
+)
+
+User.create!(
+  username: 'Maksym',
+  email: 'maksym@test.com',
+  password: '123456',
+  date_joined: Date.today,
+  avatar_url: "https://kitt.lewagon.com/placeholder/users/habidulin"
+)
+
+User.create!(
+  username: 'Emma',
+  email: 'emma@test.com',
+  password: '123456',
+  date_joined: Date.today,
+  avatar_url: "https://kitt.lewagon.com/placeholder/users/emmvs"
+)
+
+User.create!(
+  username: 'Adam',
+  email: 'adam@test.com',
+  password: '123456',
+  date_joined: Date.today,
+  avatar_url: "https://kitt.lewagon.com/placeholder/users/AdamTomczyk"
+)
+
+puts "Created #{User.count} Users 🤷🏽 🙋🏻‍♀️ 💃🏼 👨🏼‍🌾"
+
+puts "Seeds successfully created! 🌱"
