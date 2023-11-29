903109ca89f7bded1566610bb37343d6dcadc31c
Maksym Habidulin <habidulin.m@gmail.com>
2023-11-29 17:08:19 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,9 +13,7 @@ Rails.application.routes.draw do
   # root "posts#index"
   root to: "pages#home"
 
-  
   resources :captures, only:[:new, :create, :first, :second, :success]
-  
 
   resources :pages, only:[:badges, :leaderboard, :map]
 
