dac7269f534b0c8269764d643e6e9ba35b8eea59
alberto garcia <garcia.lara.alberto@gmail.com>
2023-12-01 12:25:54 +0100

    captures controller updated and the corresponding views created

--- app/controllers/captures_controller.rb
+++ app/controllers/captures_controller.rb
@@ -1,11 +1,14 @@
 class CapturesController < ApplicationController
 
-  def show
+  def index
     @captures = Capture.all
   end
 
+  def show
+    @capture = Capture.find(params[:id])
+  end
+
   def new
-    # creates a new instance of the a capture
     @new_capture = Capture.new
   end
 
@@ -21,62 +24,60 @@ class CapturesController < ApplicationController
         accept: "application/json"
       }
     )
-    # Saving the response in a hash and transforming the keys to symbols
+
     @bird_hash = JSON.parse(response.body)
-    #.transform_keys{ |key| key.to_sym }
+
     if @bird_hash["bird_detected"]
 
       @bird_scientific_name = @bird_hash["first_likely_bird_species"]["scientific_name"] 
       @bird_from_db = Bird.find_by(scientific_name: @bird_scientific_name)
-      
+
       unless @bird_from_db.nil?
         puts "bird is in our db"
-        # If this is true, the bird is already in our Birds table and we
-        # only create a capture
+        # The bird is in Birds table
         create_capture(@bird_hash)
         @new_capture.save
-        redirect_to capture_path(@new_capture)
+        redirect_to first_capture_path(@new_capture)
       else
         puts "bird has  be created"
 
         ## UPLOAD PIC TO CLOUDINARY TO GET THE KEY
 
-        # we create a new bird in the bird table
+        # we create a new bird in the bird table and a capture
         create_bird(@bird_hash)
         @new_bird.save
-        # we create a new capture
         create_capture(@new_bird)
         @new_capture.save
-        # this needs to go to the analysing page :first?
+
         redirect_to first_capture_path(@new_capture)
+        # Instance method ?
+        
       end
     else
       # Show error if it's not found
       redirect_to error_path
     end
-
-    # Now we have to do the loading pages trick
-    redirect_to capture_path(@new_capture)
   end
 
   def create_bird(bird)
-        @new_bird = Bird.new(
-          common_name: @bird_hash["first_likely_bird_species"]["common_name"],
-          scientific_name: @bird_scientific_name,
-          description:@bird_hash["first_likely_bird_species"]["description"],
-          habitat:@bird_hash["first_likely_bird_species"]["habitat"],
-          conservation_status:@bird_hash["first_likely_bird_species"]["conservation_status"],
-          migrates:@bird_hash["first_likely_bird_species"]["migrates"],
-          experience_points:@bird_hash["first_likely_bird_species"]["experience_points"],
-          common_location:@bird_hash["first_likely_bird_species"]["common_location"],
-          bird_habitat_type:@bird_hash["first_likely_bird_species"]["bird_habitat_type"],
-          bird_size:@bird_hash["first_likely_bird_species"]["bird_size"],
-          diet:@bird_hash["first_likely_bird_species"]["diet"],
-          max_velocity:@bird_hash["first_likely_bird_species"]["max_velocity"],
-          rarity:@bird_hash["first_likely_bird_species"]["rarity"],
-          sound_url:@bird_hash["first_likely_bird_species"]["sound_url"],
-          weight:@bird_hash["first_likely_bird_species"]["weight"],
-        )
+    @new_bird = Bird.new(
+      #  add the safeguard
+      common_name: @bird_hash["first_likely_bird_species"]["common_name"],
+      scientific_name: @bird_scientific_name,
+      description:@bird_hash["first_likely_bird_species"]["description"],
+      habitat:@bird_hash["first_likely_bird_species"]["habitat"],
+      conservation_status:@bird_hash["first_likely_bird_species"]["conservation_status"],
+      migrates:@bird_hash["first_likely_bird_species"]["migrates"] || 0..20.sample,
+      experience_points:@bird_hash["first_likely_bird_species"]["experience_points"],
+      common_location:@bird_hash["first_likely_bird_species"]["common_location"],
+      bird_habitat_type:@bird_hash["first_likely_bird_species"]["bird_habitat_type"],
+      bird_size:@bird_hash["first_likely_bird_species"]["bird_size"],
+      diet:@bird_hash["first_likely_bird_species"]["diet"],
+      max_velocity:@bird_hash["first_likely_bird_species"]["max_velocity"],
+      rarity:@bird_hash["first_likely_bird_species"]["rarity"],
+      sound_url:@bird_hash["first_likely_bird_species"]["sound_url"],
+      weight:@bird_hash["first_likely_bird_species"]["weight"],
+    )
   end
 
   def create_capture(bird)
@@ -101,6 +102,18 @@ class CapturesController < ApplicationController
 
     # Go to the success screen
     def first_capture
+      redirect_to capture_path(@new_capture)
+    end
 
+    def reward
+      @capture = Capture.find(params[:id])
+      @captured_bird = @capture.bird
+      total_points = @captured_bird.rarity * @captured_bird.experience_points
+      @user = current_user
+      current_user_points = @user.total_experience
+      added_points = current_user_points += total_points
+      #  chech how the entry is called an the data type
+      @user.update(total_experience: added_point)
     end
 end
+#
