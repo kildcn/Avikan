2328a62d6958bc0de54064a21a6114af3187d2a3
alberto garcia <garcia.lara.alberto@gmail.com>
2023-12-05 14:15:55 +0100

    more info button in captures/show works as intended

--- app/javascript/controllers/more_info_button_controller.js
+++ app/javascript/controllers/more_info_button_controller.js
@@ -1,22 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["extrainfo", "plusbutton"];
+  static targets = ["extrainfo", "plusbutton", "birdinfo"];
   connect() {
+    console.log("yes,it is connected")
   }
 
   more() {
     this.extrainfoTarget.classList.toggle("d-none");
     const button = this.plusbuttonTarget;
-    const buttonText = button.textContent.trim();
 
-    if (buttonText === "+") {
-      button.textContent = "-";
-      this.additionalInfoTarget.classList.add("show");
+    if (button.classList.contains("inactive")) {
+      this.birdinfoTarget.classList.toggle("bird-card")
+      this.extrainfoTarget.classList.add("show");
+      button.classList.toggle("inactive");
+      button.innerHTML = `<i class="fa-solid fa-minus" style="color: #fcca49;"></i>`;
       
     } else {
-      button.textContent = "+";
-      this.additionalInfoTarget.classList.remove("show");
+      this.birdinfoTarget.classList.toggle("bird-card")
+      this.extrainfoTarget.classList.remove("show");
+      button.classList.toggle("inactive");
+      button.innerHTML = `<i class="fa-solid fa-plus" style="color: #fcca49;"></i>`;
     }
   }
 }
