311f1cd0e5f037e68541af85f36432862accbd72
Maksym Habidulin <habidulin.m@gmail.com>
2023-12-05 16:33:34 +0100

    last part

--- app/javascript/controllers/badges.js
+++
@@ -1,9 +0,0 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   const badges = document.querySelectorAll('.badge');
-
-//   badges.forEach(badge => {
-//     if (!badge.classList.contains('unlocked')) {
-//       badge.style.opacity = '0.5';
-//     }
-//   });
-// });
