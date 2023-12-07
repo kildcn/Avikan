545be7b525e979df003f668d5c9df619b01367a1
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2023-12-07 15:11:57 +0100

    little animation delay added as well badges pictures replaced

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2023_12_07_104312) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,6 +70,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_07_104312) do
     t.string "title"
     t.text "description"
     t.string "image_url"
+    t.string "avatar"
+    t.datetime "date"
   end
 
   create_table "spots", force: :cascade do |t|
